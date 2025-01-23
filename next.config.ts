2c7e605dea5cceee3803b96549022e5107d8b7aa
Hina-naureen <hina.naureen0102@gmail.com>
2025-01-23 14:01:06 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/products/old-page',
+        destination: '/products',
+        permanent: true,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
